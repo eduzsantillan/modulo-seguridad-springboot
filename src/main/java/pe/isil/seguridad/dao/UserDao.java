f47c92982c1fea223ceeee7bfdb5143453ff2c17
Edu Zuñiga <edu@MacBook-Pro-2.local>
2022-10-12 10:33:30 -0500

    SPRING MVC: Sesion 7

---
+++ src/main/java/pe/isil/seguridad/dao/UserDao.java
@@ -0,0 +1,45 @@
+package pe.isil.seguridad.dao;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Repository;
+import pe.isil.seguridad.model.User;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+
+    public List<User> getUsers() throws Exception {
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection connection = null;
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/moduloseg","root","budakar01");
+
+        Statement st = connection.createStatement();
+
+        ResultSet rs = st.executeQuery("select name,lastname,mail,birthdate from users");
+        List<User> users = new ArrayList<>();
+
+        int count= 1;
+        while(rs.next()){
+            User user = new User(
+                    count,
+                    rs.getString("name"),
+                    rs.getString("lastname"),
+                    rs.getString("mail"),
+                    "",
+                    LocalDate.now()
+            );
+            count++;
+            users.add(user);
+        }
+        return users;
+    }
+
+}
---
+++ src/main/java/pe/isil/seguridad/service/UserService.java
@@ -0,0 +1,8 @@
+package pe.isil.seguridad.service;
+
+import pe.isil.seguridad.model.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsers();
+}
